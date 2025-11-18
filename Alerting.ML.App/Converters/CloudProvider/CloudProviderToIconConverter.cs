439362531f662441ae22c9a843b590e0e71f8119
Oleksandr Redka <redka2av@gmail.com>
2025-11-18 18:42:34 +0100

    Next round of overview page polishing. Fixing text color in cards. Adjusting hover behavior of the card. Adjusting styles and background colors of the window and buttons.

---
+++ Alerting.ML.App/Converters/CloudProvider/CloudProviderToIconConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Alerting.ML.App.Converters.CloudProvider;
+
+public class CloudProviderToIconConverter:IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
